b783d26a00715fd399878970d3869a3e332de09e
Nex <40592339+NexWasTaken@users.noreply.github.com>
2025-03-14 15:09:42 +0530

    finish chain, making way to RAG

--- README.md
+++ README.md
@@ -1,3 +1,30 @@
 # Learning Langchain
 
 Examples with langchain following the tutorial @ [LangChain Master Class For Beginners 2024 [+20 Examples, LangChain V0.2]](https://www.youtube.com/watch?v=yF9kGESAi3M)
+
+## Topic Covered
+
+### 1. Chat Models
+
+### 2. Prompt Templates
+
+### 3. Chains
+
+### 4. RAG
+
+- Storing a document/knowledge base:
+
+  - Split the document into chunks.
+  - Create embeddings for every chunk.
+    - ℹ️ What are embeddings?
+    - Simply numerical representation of text.
+  - Store embeddings in vector database or store.
+
+- Querying the document/knowledge base:
+
+  - Convert the query into vector embeddings.
+  - Call the DB Retriever to match the query embeddings vs the document chunk embeddings.
+
+- Add metadata for better answers.
+
+### 5. Agents & Tools
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from dotenv import load_dotenv
-from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_google_genai import ChatGoogleGenerativeAI, GoogleGenerativeAIEmbeddings
 from langchain_core.messages import AIMessage, HumanMessage, SystemMessage
 from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnableLambda, RunnableParallel, RunnableBranch
 
 
 def chat_model_basic():
@@ -50,10 +52,14 @@ def prompt_template_basic():
 
     print(prompt)
 
+
 def prompt_template_with_chat_model():
     model = ChatGoogleGenerativeAI(model="gemini-1.5-pro")
     messages = [
-        ("system", "You are a comedian who tells scary, terrifying and creepy jokes that leave the user with unease."),
+        (
+            "system",
+            "You are a comedian who tells scary, terrifying and creepy jokes that leave the user with unease.",
+        ),
         HumanMessage("Hello, Comedian! What is your name? What do you do?"),
         AIMessage(
             "I am Art the Clown. I tell creepy and scary jokes that will leave you more scared than amused."
@@ -67,13 +73,186 @@ def prompt_template_with_chat_model():
     print(r.content)
 
 
+def chains_basics():
+    model = ChatGoogleGenerativeAI(model="gemini-1.5-pro")
+    prompt_template = ChatPromptTemplate.from_messages(
+        [
+            ("system", "You are a comedian who tells jokes about {joke_topic}."),
+            ("human", "Tell me {joke_count} jokes."),
+        ]
+    )
+
+    chain = prompt_template | model | StrOutputParser()
+
+    r = chain.invoke({"joke_topic": "lawyers", "joke_count": 3})
+    print(r)
+
+
+def chains_extended():
+    model = ChatGoogleGenerativeAI(model="gemini-1.5-pro")
+    prompt_template = ChatPromptTemplate.from_messages(
+        [
+            ("system", "You are a comedian who tells jokes about {joke_topic}."),
+            ("human", "Tell me {joke_count} jokes."),
+        ]
+    )
+
+    uppercase_output = RunnableLambda(lambda x: x.upper())
+    count_words = RunnableLambda(lambda x: f"Word count: {len(x.split())}\n{x}")
+
+    chain = prompt_template | model | StrOutputParser() | uppercase_output | count_words
+
+    r = chain.invoke({"joke_topic": "lawyers", "joke_count": 3})
+    print(r)
+
+
+def chains_parallel():
+    model = ChatGoogleGenerativeAI(model="gemini-1.5-pro")
+
+    prompt_template = ChatPromptTemplate.from_messages(
+        [
+            ("system", "You are an expert product reviewer."),
+            ("human", "List the main features of the product {product_name}."),
+        ]
+    )
+
+    def analyze_pros(features: str):
+        return ChatPromptTemplate.from_messages(
+            [
+                ("system", "You are an expert product reviewer."),
+                (
+                    "human",
+                    "Given these features: {features}, list the pros these features.",
+                ),
+            ]
+        ).format_prompt(features=features)
+
+    def analyze_cons(features: str):
+        return ChatPromptTemplate.from_messages(
+            [
+                ("system", "You are an expert product reviewer."),
+                (
+                    "human",
+                    "Given these features: {features}, list the cons these features.",
+                ),
+            ]
+        ).format_prompt(features=features)
+
+    pros_branch = RunnableLambda(lambda x: analyze_pros(x)) | model | StrOutputParser()
+    cons_branch = RunnableLambda(lambda x: analyze_cons(x)) | model | StrOutputParser()
+
+    chain = (
+        prompt_template
+        | model
+        | StrOutputParser()
+        | RunnableParallel(branches={"pros": pros_branch, "cons": cons_branch})
+        | RunnableLambda(
+            lambda x: f"Pros:\n{x['branches']['pros']}\n\nCons:\n{x['branches']['cons']}"
+        )
+    )
+
+    r = chain.invoke({"product_name": "MacBook Pro"})
+    print(r)
+
+
+def chains_branching():
+    model = ChatGoogleGenerativeAI(model="gemini-1.5-pro")
+
+    positive_feedback_template = ChatPromptTemplate.from_messages(
+        [
+            ("system", "You are a helpful assistant."),
+            (
+                "human",
+                "Generate a thank you note for this positive feedback: {feedback}.",
+            ),
+        ]
+    )
+    negative_feedback_template = ChatPromptTemplate.from_messages(
+        [
+            ("system", "You are a helpful assistant."),
+            (
+                "human",
+                "Generate a response addressing this negative feedback: {feedback}.",
+            ),
+        ]
+    )
+    neutral_feedback_template = ChatPromptTemplate.from_messages(
+        [
+            ("system", "You are a helpful assistant."),
+            (
+                "human",
+                "Generate a request for more details for this neutral feedback: {feedback}.",
+            ),
+        ]
+    )
+    escalate_feedback_template = ChatPromptTemplate.from_messages(
+        [
+            ("system", "You are a helpful assistant."),
+            (
+                "human",
+                "Generate a message to escalate this feedback to a human agent: {feedback}.",
+            ),
+        ]
+    )
+
+    branches = RunnableBranch(
+        (
+            lambda x: "positive" in x,
+            positive_feedback_template | model | StrOutputParser(),
+        ),
+        (
+            lambda x: "negative" in x,
+            negative_feedback_template | model | StrOutputParser(),
+        ),
+        (
+            lambda x: "neutral" in x,
+            neutral_feedback_template | model | StrOutputParser(),
+        ),
+        escalate_feedback_template | model | StrOutputParser(),
+    )
+
+    classification_template = ChatPromptTemplate.from_messages(
+        [
+            ("system", "You are a helpful assistant."),
+            (
+                "human",
+                "Classify the sentiment of this feedback as positive, negative, neutral, or escalate: {feedback}.",
+            ),
+        ]
+    )
+
+    classification_chain = classification_template | model |  StrOutputParser()
+
+    chain = classification_chain | branches
+
+    # Good review - "The product is excellent. I really enjoyed using it and found it very helpful."
+    # Bad review - "The product is terrible. It broke after just one use and the quality is very poor."
+    # Neutral review - "The product is okay. It works as expected but nothing exceptional."
+    # Default - "I'm not sure about the product yet. Can you tell me more about its features and benefits?"
+
+    review = "The product is terrible. It broke after just one use and the quality is very poor."
+    r = chain.invoke({"feedback": review})
+    print(r)
+
+
 def main():
     print("Running main function.")
     # chat_model_basic()
     # chat_model_basic_conversation()
     # chat_model_conversation_with_user()
     # prompt_template_basic()
-    prompt_template_with_chat_model()
+    # prompt_template_with_chat_model()
+    # chains_basics()
+    # chains_extended()
+    # chains_parallel()
+    # chains_branching()
+
+    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+    vector = embeddings.embed_query("I love cats more than dogs.")
+    print(f"Vector α has {len(vector)} dimensions.")
+
+    vector = embeddings.embed_query("Harry porter sucks!")
+    print(f"Vector β has {len(vector)} dimensions.")
 
 
 if __name__ == "__main__":
